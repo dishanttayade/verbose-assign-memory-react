e96d87c4d1958ec79b2196bc1fa76517975b2963
dishanttayade <dishanttayade1@outlook.com>
2021-11-06 10:16:25 +0530

    color

--- src/components/employees/Employees.js
+++ src/components/employees/Employees.js
@@ -1,7 +1,7 @@
 import edata from '../Data.json'
 const Employees = ()=> {
     return (
-        <div style={{margin:"50px", color:"white"}}>
+        <div style={{marginTop:"100px", color:"white"}} className="container text-center">
             <h1>{edata[3].title}</h1>
             {
                 edata[3].employees.map( (e) => (
